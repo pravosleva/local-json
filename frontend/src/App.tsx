c1f036fba147983e5cea9d613f61dec20df656b7
Denis Poltoratsky <dpoltoratskiy@igatec.com>
2021-02-04 15:21:17 +0300

    frontend: eslint, prettier added

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,26 +1,60 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+// @ts-ignore
+import JSONInput from 'react-json-editor-ajrm'
+// @ts-ignore
+import locale from 'react-json-editor-ajrm/locale/en'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+type TError =
+  | false
+  | {
+      token: number
+      line: number
+      reason: string
+    }
+type TObject = {
+  [key: string]: any
+}
+interface IEvent {
+  error: TError
+  jsObject: undefined | TObject
+  json: string
+  lines: number
+  markupText: string
+  plainText: string
 }
 
-export default App;
+const App: React.FC = () => {
+  const handleChange = (e: IEvent) => {
+    const {
+      // error,
+      jsObject,
+    } = e
+
+    // if (error) {
+    //   console.log(error)
+    //   return
+    // }
+    if (jsObject) {
+      console.log(jsObject)
+    }
+  }
+  return (
+    <div style={{ border: '1px solid red' }}>
+      <JSONInput
+        id="a_unique_id"
+        placeholder={{
+          a: 1,
+        }}
+        theme="light_mitsuketa_tribute"
+        colors={{
+          string: 'red', // overrides theme colors with whatever color value you want
+        }}
+        locale={locale}
+        height="550px"
+        onChange={handleChange}
+      />
+    </div>
+  )
+}
+
+export default App
